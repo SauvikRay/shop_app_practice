eedf7e95e0974055c0f2f4c167b95a14f78a0a06
SauvikRay <sauvikray001@gmail.com>
2022-03-22 18:42:27 +0600

    Order Item Update to firebase

--- lib/provider/orders.dart
+++ lib/provider/orders.dart
@@ -1,40 +1,58 @@
+import 'dart:convert';
+
 import 'package:flutter/widgets.dart';
 import '../provider/cart.dart' show CartItem;
+import 'package:http/http.dart' as http;
 
-class OrderItem{
+class OrderItem {
   final String id;
   final double amount;
   final List<CartItem> products;
   final DateTime dateTime;
 
-  OrderItem({Key? key,
-              required this.id, 
-              required this.amount, 
-              required this.products, 
-              required this.dateTime});
-  
+  OrderItem(
+      {Key? key,
+      required this.id,
+      required this.amount,
+      required this.products,
+      required this.dateTime});
 }
 
+class Orders with ChangeNotifier {
+  final List<OrderItem> _orders = [];
 
-class Orders with ChangeNotifier{
-
- final List<OrderItem> _orders=[];
-
-    List<OrderItem> get orders{
-      return [..._orders];
-    }
-  
-  void addOrder(List<CartItem> cartProducts, double total){
-      _orders.insert(
-            0, 
-            OrderItem(
-              id: DateTime.now().toString(), 
-              amount: total, 
-              products: cartProducts, 
-              dateTime: DateTime.now(),),);
-      
-      notifyListeners();
-
+  List<OrderItem> get orders {
+    return [..._orders];
   }
 
+  Future<void> addOrder(List<CartItem> cartProducts, double total) async {
+    final timeStamp = DateTime.now();
+    final response = await http.post(
+        //Working Code
+        Uri.parse(
+            'https://shopapp-e73fe-default-rtdb.asia-southeast1.firebasedatabase.app/orders.json'),
+        body: jsonEncode({
+          'amount': total,
+          'dateTime': timeStamp.toIso8601String(),
+          'products': cartProducts
+              .map((cartProduct) => {
+                    'id': cartProduct.id,
+                    'title': cartProduct.title,
+                    'quantity': cartProduct.quantity,
+                    'price': cartProduct.price
+                  })
+              .toList(),
+        }),);
+    _orders.insert(
+      0,
+      OrderItem(
+        id: json.decode(response.body)['name'], //DateTime.now().toString(),
+        amount: total,
+        products: cartProducts,
+        dateTime: timeStamp,
+      ),
+    );
+
+    notifyListeners();
+  }
 }
--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -5,7 +5,6 @@ import '../provider/orders.dart';
 import '../provider/cart.dart' show Cart;
 import '../widgets/cart_item.dart';
 
-
 class CartScreen extends StatelessWidget {
   const CartScreen({Key? key}) : super(key: key);
 
@@ -13,58 +12,103 @@ class CartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-  final cart= Provider.of<Cart>(context,listen: false);
+    final cart = Provider.of<Cart>(context, listen: false);
 
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(title:const Text('Your Cart')),
+        appBar: AppBar(title: const Text('Your Cart')),
         body: Column(
-          children:<Widget> [
+          children: <Widget>[
             //Card for Total
             Card(
-              margin:const EdgeInsets.all(15.0),
+              margin: const EdgeInsets.all(15.0),
               child: Padding(
                 padding: const EdgeInsets.all(8),
-                child:Row(
+                child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget> [
-              const Text('Total', style: TextStyle(fontSize:20,),),
-              const Spacer(),
-               Chip(
-                 label: Text('\$${cart.totalAmount.toStringAsFixed(2)}',style:const TextStyle(color:Colors.white,),),
-                 backgroundColor: Colors.deepPurple,
-               ),
-               // CartItem Count ==0 then remove the order now button
-               (cart.itemCount == 0)?
-                  Container() : TextButton(
-                 child:const Text('ORDER NOW',style: TextStyle(color:Colors.indigoAccent,fontWeight: FontWeight.bold),),
-                 onPressed: (){
-                   Provider.of<Orders>(context,listen: false).addOrder(cart.items.values.toList(), cart.totalAmount);
-                   cart.crearCard();
-                 }, 
-                 )  
-              ] ,),
+                  children: <Widget>[
+                    const Text(
+                      'Total',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                    const Spacer(),
+                    Chip(
+                      label: Text(
+                        '\$${cart.totalAmount.toStringAsFixed(2)}',
+                        style: const TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      backgroundColor: Colors.deepPurple,
+                    ),
+                    OrderButton(cart: cart),
+                    
+                  ],
+                ),
+              ),
             ),
+            //Card For Order Items
+            Expanded(
+              child: ListView.builder(
+                itemCount: cart.items.length,
+                itemBuilder: (ctx, index) {
+                  return CartItem(
+                    //Convert to a MAP because the items is a MAP
+                    id: cart.items.values.toList()[index].id,
+                    productId: cart.items.keys.toList()[index],
+                    price: cart.items.values.toList()[index].price,
+                    quantity: cart.items.values.toList()[index].quantity,
+                    title: cart.items.values.toList()[index].title,
+                  );
+                },
+              ),
             ),
-          //Card For Order Items
-          Expanded(child: ListView.builder(
-            itemCount: cart.items.length,
-            itemBuilder: (ctx,index){
-              return CartItem(
-                //Convert to a MAP because the items is a MAP
-               id: cart.items.values.toList()[index].id,
-               productId: cart.items.keys.toList()[index],
-               price: cart.items.values.toList()[index].price,
-               quantity: cart.items.values.toList()[index].quantity,
-               title: cart.items.values.toList()[index].title,
-              );
-                
-            },
-            ),),
           ],
         ),
       ),
+    );
+  }
+}
+
+class OrderButton extends StatefulWidget {
+  final Cart cart;
+  const OrderButton({
+    Key? key,
+    required this.cart,
+  }) : super(key: key);
+
+  @override
+  State<OrderButton> createState() => _OrderButtonState();
+}
+
+class _OrderButtonState extends State<OrderButton> {
+
+  bool _isLoading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      child: _isLoading ? const CircularProgressIndicator():  Text(
+        'ORDER NOW',
+        style:
+            TextStyle(color: (_isLoading ||widget.cart.totalAmount <= 0)? Colors.indigoAccent.withOpacity(0.3): Colors.indigoAccent , fontWeight: FontWeight.bold),
+      ),
+      // CartItem Count ==0 then remove the order now button.
+      onPressed:( widget.cart.totalAmount <= 0 || _isLoading)
+          ? null
+          : () async {
+            setState(() {
+              _isLoading = true;
+            });
+             await Provider.of<Orders>(context, listen: false)
+                  .addOrder(widget.cart.items.values.toList(), widget.cart.totalAmount);
+              widget.cart.crearCard();
+              setState(() {
+              _isLoading = false;
+            });
+            },
     );
   }
 }
